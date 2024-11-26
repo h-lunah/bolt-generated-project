abc22b2d435aca26ea149f2a7eb19a70cf37624b
luna <71488561+h-lunah@users.noreply.github.com>
2024-11-26 09:18:31 +0100

    Initial commit from your app

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+export default function Home() {
+      return (
+        <div className="flex items-center justify-center min-h-screen bg-gray-100">
+          <h1 className="text-4xl font-bold text-center text-blue-600">
+            Hello, World!
+          </h1>
+        </div>
+      )
+    }
---
+++ script.js
@@ -0,0 +1,2 @@
+// This file can be used for any JavaScript logic if needed.
+    // For this simple example, no JavaScript is required.
